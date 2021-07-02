4a681528a257dae5f93b604730c1e32392ee72a1
Advait Deodhar <advait.deodhar@appzen.com>
2021-07-02 22:19:19 +0530

    Easy Problem Solving on Hacker-rank

---
+++ hacker-rank/src/main/java/easy/UtopianTree.java
@@ -0,0 +1,62 @@
+package easy;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class UtopianTree {
+
+    /*
+     * Complete the 'utopianTree' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int utopianTree(int n) {
+        int complete = n/2;
+        int ans = 1;
+        for ( int i = 0 ; i < complete; i++) {
+            ans <<= 1;
+            ans += 1;
+        }
+
+        if ( n % 2 == 1) {
+            ans <<= 1;
+        }
+        return ans;
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = utopianTree(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
