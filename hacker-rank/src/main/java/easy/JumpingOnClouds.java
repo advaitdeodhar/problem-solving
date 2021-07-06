4951806ec8728cb18d7191cdabbdd9d523284017
Advait Deodhar <advait.deodhar@appzen.com>
2021-07-06 19:39:22 +0530

    Problem solving continued....

---
+++ hacker-rank/src/main/java/easy/JumpingOnClouds.java
@@ -0,0 +1,67 @@
+package easy;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class JumpingOnClouds {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        int count = 0;
+        int load = 0;
+        boolean skip = true;
+        for ( Integer i : c ) {
+            if ( skip ) {
+                skip = false;
+                continue;
+            }
+            load++;
+            if ( load == 2 ) {
+                count++;
+                load = i;
+            }
+        }
+        count += load;
+        return count;
+    }
+
+
+
+}
+
+class SolutionJump {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                .map(Integer::parseInt)
+//                .collect(toList());
+//
+//        int result = Result.jumpingOnClouds(c);
+//
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+        System.out.println(JumpingOnClouds.jumpingOnClouds(new ArrayList<>(Arrays.asList(0,0,0,0,0,0))));
+    }
+}
